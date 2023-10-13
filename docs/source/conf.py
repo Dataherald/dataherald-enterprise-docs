e330a4e58e6402b677f7d22a6b5eed289400f88f
Juan Carlos Jose Camacho <jc@dataherald.com>
2023-10-13 13:38:43 -0600

    Add sphinx book theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,11 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
-
-release = '0.1'
-version = '0.1.0'
+project = 'Dataherald Enterprise'
+copyright = "2023, Dataherald"
+author = "Dataherald"
+release = "main"
+version = '0.0.1'
 
 # -- General configuration
 
@@ -29,7 +28,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_book_theme"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
